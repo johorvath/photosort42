222606c37b5961c622327544d59c95f7bc406a57
evi92 <eva_witzel@web.de>
2014-10-19 20:09:50 +0200

    added File Dialog functionality; show selected comparative picture on GUI

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFileDialog>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
 }
 
 MainWindow::~MainWindow()
@@ -15,5 +17,33 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_sort_clicked()       //Sortierbutton
 {
+
+}
+
+
+void MainWindow::on_pushButton_compareInput_clicked()
+{
+    QString imgOne=QFileDialog::getOpenFileName(
+                this,
+                "Select an image to sort according to one person",
+                "/home",
+                "Images (*.png *.xpm *.jpg)");
+
+    QPixmap pix(imgOne);
+    int w = ui->label_compareImage->width();
+    int h = ui->label_compareImage->height();
+    ui ->label_compareImage->setPixmap(pix.scaled(w,h,Qt::KeepAspectRatio));
+
+
+}
+
+
+void MainWindow::on_pushButton_photoInput_clicked()
+{
+    QStringList img=QFileDialog::getOpenFileNames(
+                this,
+                "Select more than one images to sort",
+                "/home",
+                "Images (*.png *.xpm *.jpg)");
 
 }
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -16,7 +16,11 @@ public:
     ~MainWindow();
 
 private slots:
-    void on_pushButton_sort_clicked();
+    void on_pushButton_sort_clicked();    
+
+    void on_pushButton_compareInput_clicked();
+
+    void on_pushButton_photoInput_clicked();
 
 private:
     Ui::MainWindow *ui;
